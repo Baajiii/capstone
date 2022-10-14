f6b302739dffd636cd230042dc391550dd075882
Baajiii <samsudeenbasith00@gmail.com>
2022-10-14 20:09:58 +0530

    Testing

--- AppTesting/src/com/Testing/main.java
+++ AppTesting/src/com/Testing/main.java
@@ -45,38 +45,76 @@ public class main {
 	
 	@Test(priority=1)
 	public void ApplyAdharCrad() throws InterruptedException {
-		String route = "adminhome";
-		webdriver.get(url + "/" + route);
 		
 		webdriver.findElement(By.id("apply")).click();
 		System.out.println(" enter successfully");
-//		
-//		webdriver.findElement(By.id("name")).sendKeys("basith");		
-//		System.out.println("Name Enter successfully");
-//		
-//		webdriver.findElement(By.id("dob")).sendKeys("12/10/2000");		
-//		System.out.println("dob Enter successfully");
-//		
-//		webdriver.findElement(By.id("adress")).sendKeys("xyz");		
-//		System.out.println("Address Enter successfully");
-//
-//		webdriver.findElement(By.id("email")).sendKeys("baaji@gmail.com");		
-//		System.out.println("Email Enter successfully");
-//		
-//		webdriver.findElement(By.id("mobileNumber")).sendKeys("9876543210");		
-//		System.out.println("Number Enter successfully");
-//		webdriver.findElement(By.id("gender")).sendKeys("male");		
-//		System.out.println("gender Enter successfully");
-		JavascriptExecutor js = (JavascriptExecutor) webdriver;
-		 
-	       js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-//		
-//		webdriver.findElement(By.id("approve")).click();
-//		System.out.println("applied successfully");
+		
+		webdriver.findElement(By.id("name")).sendKeys("basith");		
+		System.out.println("Name Enter successfully");
+		
+		webdriver.findElement(By.id("dob")).sendKeys("12/10/2000");		
+		System.out.println("dob Enter successfully");
+		
+		webdriver.findElement(By.id("address")).sendKeys("xyz");		
+		System.out.println("Address Enter successfully");
+
+		webdriver.findElement(By.id("email")).sendKeys("baaji@gmail.com");		
+		System.out.println("Email Enter successfully");
+		
+		webdriver.findElement(By.id("mobileNumber")).sendKeys("9876543210");		
+		System.out.println("Number Enter successfully");
+		webdriver.findElement(By.id("gender")).sendKeys("male");		
+		System.out.println("gender Enter successfully");
+
+		
+		webdriver.findElement(By.name("register")).click();
+		System.out.println("registered successfully");
 		System.out.println();
 		System.out.println();
 		Thread.sleep(2000);
 
 
 	}
+	
+	@Test(priority=2)
+	public void Citizenlogin() throws InterruptedException {
+	
+		
+		webdriver.findElement(By.id("mobileNumber")).sendKeys("8667413704");		
+		System.out.println("email Enter successfully");
+
+		webdriver.findElement(By.id("password")).sendKeys("123456");
+		System.out.println("Password enter successfully");
+		
+		webdriver.findElement(By.name("login")).click();
+		System.out.println("Citizen login successfully");
+		System.out.println();
+		System.out.println();
+		Thread.sleep(2000);
+		
+
+	}
+	
+	
+	@Test(priority=3)
+	public void ApplyDuplicate() throws InterruptedException {
+	
+		
+		webdriver.findElement(By.id("userupdate")).click();;		
+		System.out.println("Enter successfully");
+
+		webdriver.findElement(By.id("update")).click();;
+		System.out.println("Updated successfully");
+		
+		webdriver.findElement(By.id("edit")).click();
+		System.out.println("Edited successfully");
+		System.out.println();
+		System.out.println();
+		Thread.sleep(2000);
+		
+
+	}
+	
+	
+	
 }
